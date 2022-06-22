1a58e33c29e2257d8643d84a35f46d5617303d39
Daiya Mita <daiyamaxus@gmail.com>
2022-06-23 00:14:48 +0900

    first commit

---
+++ app/index.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, make_response
+import pathlib
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+  files = pathlib.Path('files')
+  file_list = [{'title': file.name, 'price': f'/download/{file.name}'} for file in files.iterdir() if file.is_file()]
+  return render_template(
+    'index.html',
+    books=file_list
+  )
+
+
+@app.route("/download/<filename>")
+def downloadzip(filename):
+    response = make_response()
+    response.data  = open(f'./files/{filename}', "rb").read()
+    response.headers['Content-Type'] = 'application/octet-stream'
+    response.headers['Content-Disposition'] = f'attachment; filename={filename}'
+    return response
+
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=80, debug=True)
